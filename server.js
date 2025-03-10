2d9e95451351c406f72621f1d81d9836f9b6f1e3
Shinra Kusakabe <73338956+sasmithaK@users.noreply.github.com>
2025-03-10 10:56:40 +0530

    chore: configure MongoDB connection

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
+const dotenv = require('dotenv'); // Global enviroment veriables
+const morgan = require('morgan'); // Logger
+const colors = require('colors'); // Colours for console.log
+const connectDB = require('./config/db')
+
+dotenv.config({ path: '.env' });
+
+connectDB();
+
+const transactions = require('./routes/transactions');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use('/api/transactions', transactions);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000'.yellow.bold);
-});
