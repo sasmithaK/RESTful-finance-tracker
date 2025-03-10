5d18d85d775f24fe85f4aa9db46e43b365f27567
Shinra Kusakabe <73338956+sasmithaK@users.noreply.github.com>
2025-03-10 20:19:52 +0530

    style: refactored code structure for better readability

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const dotenv = require('dotenv'); // Global enviroment veriables
 const morgan = require('morgan'); // Logger
 const colors = require('colors'); // Colours for console.log
 const connectDB = require('./config/db')
+const app = express();
+
+const transactions = require('./routes/transactions');
 
 dotenv.config({ path: '.env' });
 
 connectDB();
 
-const transactions = require('./routes/transactions');
-
-const app = express();
-
 app.use('/api/transactions', transactions);
+
 
 const PORT = process.env.PORT || 3000;
 
