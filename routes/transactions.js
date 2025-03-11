2680ad48246852b83afb3aa72f6008f59960ecca
Shinra Kusakabe <73338956+sasmithaK@users.noreply.github.com>
2025-03-11 14:41:04 +0530

    feature: add controllers and routes for PUT and DELETE

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions, addTransactions, deleteTransactions } = require('../controllers/transactionController')
+const { getTransactions, addTransactions, updateTransaction, deleteTransactions } = require('../controllers/transactionController');
 
 router
     .route('/')
     .get(getTransactions)
     .post(addTransactions);
+
+router
+    .route('/:id')
+    .put(updateTransaction)
+    .delete(deleteTransactions);
 
 module.exports = router;
