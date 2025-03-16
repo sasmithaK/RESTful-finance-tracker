e4a4e127e7a272af4ac8f1e7f3cf5fbe8a5b9d44
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2025-03-16 14:52:53 +0530

    temp

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use('/api/transactions', transactions);
 app.use('/api/auth', authentication);
 app.use('/api/budgets', budget);
-
+      
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
